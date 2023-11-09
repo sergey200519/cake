ce8f7d41d7c36938974e1235a91c06f23d4abd4e
sergey <oborok05@bk.ru>
2023-11-09 10:09:25 +0300

    reset

--- cake_shop/adminapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from adminapp.models import Applications
-
-# Register your models here.
-admin.site.register(Applications)
--- cake_shop/mainapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from mainapp.models import ProductCategories, Products, ImgProducts
-
-admin.site.register(ProductCategories)
-admin.site.register(Products)
-admin.site.register(ImgProducts)
